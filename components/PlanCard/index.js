344fd5cc5167f1102965b8a2d1973318b90e9988
Hanming Zhu <hanming@huawen.com>
2022-01-08 14:28:21 +0800

    Improve plan card spacing

--- components/PlanCard/index.js
+++ components/PlanCard/index.js
@@ -7,10 +7,10 @@ function PlanCard({ plan }) {
       className="flex flex-col p-2 rounded-lg sidebar-card"
       style={{ backgroundColor: '#201F28' }}
     >
-      <div className="text-s font-semibold mb-2 sidebar-card-title">
+      <div className="text-s font-semibold mb-1 sidebar-card-title">
         {plan.title}
       </div>
-      <div className="text-xs mb-2">{plan.description}</div>
+      <div className="text-xs mb-3">{plan.description}</div>
       <div className="flex">
         <div className="flex mr-2 text-xs" style={{ color: '#7B7B81' }}>
           <Star className="mr-1" /> {plan.numOfLikes}
